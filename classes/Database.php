c438a841eb55d7b2e1baccb3d6fd933a37b5d070
Rifky <aaliyaaousama@gmail.com>
2018-09-13 08:18:50 +0700

    Database Connection and Singleton Pattern

--- classes/Database.php
+++ classes/Database.php
@@ -1,1 +1,30 @@
-Database.php
+<?php
+
+class Database{
+
+    private static $INSTANCE;
+    private $mysqli,
+            $HOST = 'localhost',
+            $USER = 'root',
+            $PASS = '123',
+            $DBNAME = 'tutorial_php';
+
+    public function __construct()
+    {
+
+        $this->mysqli = new mysqli($this->HOST, $this->USER, $this->PASS, $this->DBNAME);
+        if ($this->mysqli == mysqli_connect_error()) {
+            echo 'gagal koneksi ke database';
+        }
+    }
+
+    public static function getInstance()
+    {
+        if ( !isset(self::$INSTANCE)) {
+            self::$INSTANCE = new Database();
+        }
+        return self::$INSTANCE;
+    }
+}
+
+?>
